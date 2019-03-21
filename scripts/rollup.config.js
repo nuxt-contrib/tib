f8c6173c9bb4dc3f73dfd66bd9d21910f17b5ffe
pimlie <pimlie@hotmail.com>
2019-03-21 17:30:47 +0100

    chore: improve build
    dont inline dynamic imports
    
    always cleanup lib dir
    
    set node builtins as externals

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,22 +1,30 @@
+import path from 'path'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import json from 'rollup-plugin-json'
 import defaultsDeep from 'lodash.defaultsdeep'
 
+import { builtins } from './builtins'
+
 const pkg = require('../package.json')
 
 function rollupConfig({
+  external = [],
   plugins = [],
   ...config
   } = {}) {
 
   return defaultsDeep({}, config, {
-    inlineDynamicImports: true,
-    external: Object.keys(pkg.dependencies),
+    inlineDynamicImports: false,
+    external: [
+      ...builtins,
+      ...Object.keys(pkg.dependencies),
+      ...external
+    ],
     input: 'src/index.js',
     output: {
       name: 'tib',
-      file: pkg.main,
+      dir: path.dirname(pkg.main),
       format: 'cjs',
       sourcemap: false
     },

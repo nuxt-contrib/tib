d62e9356e63d42d18109e9c6937fa925bcde1617
pimlie <pimlie@hotmail.com>
2019-03-26 16:20:43 +0100

    feat: rename browser export to createBrowser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import Browser from './browser'
 import * as commands from './utils/commands'
 
-export async function browser(str, conf, autoStart = true) {
+export async function createBrowser(str, conf, autoStart = true) {
   const instance = Browser.get(str, conf)
   if (!autoStart) {
     return instance
   }
 
   return (await instance).start()
+}
+
+export function browser(...args) {
+  console.warn(`DeprecationWarning: 'browser' has been renamed to 'createBrowser'`)
+  return createBrowser(...args)
 }
 
 export {
